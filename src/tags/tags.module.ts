99a7af36f72a33d34d25abdf26a605f4eb36a8cd
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2026-09-19 21:43:12 +0300

    feat: add tags and meta-option tables

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TagsService } from './tags.service';
 import { TagsController } from './tags.controller';
+import { Tag } from './entities/tag.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [TagsController],
   providers: [TagsService],
+  imports: [TypeOrmModule.forFeature([Tag])],
 })
 export class TagsModule {}
