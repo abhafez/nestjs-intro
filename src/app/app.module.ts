d47fe5ac9e8bdb17bd81c822e683c3ce2cc95ed3
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2026-09-20 20:34:05 +0300

    feat: add environment-based configuration and validation
    - Introduced environment validation using Joi schema.
    - Added `database.config.ts` and `app.config.ts` for structured configuration.
    - Updated `ConfigModule` setup in `AppModule` to load configs and validation schema.
    - Enhanced `start:dev` script to include `NODE_ENV=development`.
    - Added `.env.development` for local environment configuration.
    - Installed `joi` library for validation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,11 @@ import { formatValidationErrors } from './i18n/format-validation-errors';
 import { AppEntities } from './app.entities';
 import { MetaOptionModule } from '../meta-option/meta-option.module';
 import { TagsModule } from '../tags/tags.module';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import enviromentValidation from './config/enviroment.validation';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
   controllers: [AppController],
@@ -26,23 +31,32 @@ import { TagsModule } from '../tags/tags.module';
     AppService,
     {
       provide: APP_PIPE,
-      useValue: new ValidationPipe({ transform: true, whitelist: true, exceptionFactory: i18nValidationErrorFactory }),
+      useValue: new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        exceptionFactory: i18nValidationErrorFactory,
+      }),
     },
     { provide: APP_FILTER, useValue: new I18nValidationExceptionFilter({ errorFormatter: formatValidationErrors }) },
   ],
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      load: [appConfig, databaseConfig],
+      validationSchema: enviromentValidation,
+    }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
+      useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: config.get<string>('DATABASE_HOST'),
-        port: config.get<number>('DATABASE_PORT'),
-        username: config.get<string>('DATABASE_USER'),
-        password: config.get<string>('DATABASE_PASSWORD'),
-        database: config.get<string>('DATABASE_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
+        synchronize: configService.get('database.synchronize'),
+        port: configService.get('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        host: configService.get('database.host'),
+        autoLoadEntities: configService.get('database.autoLoadEntities'),
+        database: configService.get('database.name'),
         entities: AppEntities,
       }),
     }),
